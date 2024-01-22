634dec0616eced2e9d27e7b401a3d54374e22ca6
Keane Grech <44116193+tixeld@users.noreply.github.com>
2024-01-22 01:53:33 +0100

    Improved coming soon page, analytics, metadata

--- app/page.tsx
+++ app/page.tsx
@@ -2,8 +2,8 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="">
-      Portfolio is a WIP
+    <main className="absolute inset-0 -z-10 h-full w-full items-center px-5 py-24 [background:radial-gradient(125%_125%_at_50%_10%,#000_40%,#63e_100%)] flex justify-center">
+        <div className="text-white font-bold text-6xl">Coming soon</div>
     </main>
   );
 }
